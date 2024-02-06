7359482871d92560f4fd69498050be9e95bc8236
Bruno Vinícius <vbruno96@gmail.com>
2024-02-06 20:37:03 -0300

    route create new poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,27 @@
 import fastify from "fastify"
+import { PrismaClient } from "@prisma/client"
+import { z } from "zod"
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return  'Hello NLW'
+const prisma = new PrismaClient()
+
+app.post('/polls', async (request, replay) => {
+  const createPollBody = z.object({
+    title: z.string()
+  })
+
+  const { title } = createPollBody.parse(request.body)
+  
+  const newPoll = await prisma.poll.create({
+    data: {
+      title
+    }
+  })
+  
+  return replay.status(201).send({
+    pollId: newPoll.id
+  })
 })
 
 app.listen({ port: 3333 }).then(() => {
