aba104cf297407f366267308e4872a9894c96e70
Bruno Vinícius <vbruno96@gmail.com>
2024-02-07 18:35:54 -0300

    show score options in route get poll

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify"
 import z from "zod";
 import { prisma } from "../../lib/prisma"
+import { redis } from "../../lib/redis";
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, replay) => {
@@ -23,9 +24,33 @@ export async function getPoll(app: FastifyInstance) {
         }
       }
     })
+
+    if (!poll) {
+      return replay.status(400).send({ message: "Poll not found" })
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES")
+
+    const votes = result.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1]
+
+        Object.assign(obj, { [line]: Number(score) })
+      }
+
+      return obj
+    }, {} as Record<string, number>)
     
     return replay.send({
-      poll
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        options: poll.options.map(option => ({
+          id: option.id,
+          title: option.title,
+          score: (option.id in votes) ? votes[option.id] : 0
+        }))
+      }
     })
   })
 }
